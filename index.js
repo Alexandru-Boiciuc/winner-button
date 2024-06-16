1539c322fefda8422537e4c04ee6fa19592ae736
Alexandru-Boiciuc <claudiub.alex@icloud.com>
2024-06-16 07:44:00 +0100

    index.js

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-let randomNumber = Math.floor(Math.random() * 3);
-for (let i = 0; i < 3; ++i) {
-    let button = document.createElement("button");
-    button.innerHTML = "Button " + (i + 1);
-    button.className = "btn btn-outline-success m-1"; 
-    button.onclick = function() {
-        if (i === randomNumber) {
-            button.innerHTML = "You win";
-            confetti({
-                particleCount: 2000,
-                spread: 10,
-                origin: { y: 0.0 },
-            });
-        } else {
-            button.innerHTML = "You lose";
-        }
-    };
-    let buttonDiv = document.getElementById("buttons");
-    buttonDiv.appendChild(button);
+function createButtons() {
+    let randomNumber = Math.floor(Math.random() * 3);
+    for (let i = 0; i < 3; ++i) {
+        let button = document.createElement("button");
+        button.innerHTML = "Button " + (i + 1);
+        button.className = "btn btn-outline-success m-1"; 
+        button.onclick = function() {
+            handleButtonClick(i, randomNumber);
+        };
+        let buttonDiv = document.getElementById("buttons");
+        buttonDiv.appendChild(button);
+    }
 }
+
 function generateButtons() {
     const num = parseInt(document.getElementById('numberInput').value);
     const container = document.getElementById('buttonContainer');
@@ -27,23 +21,26 @@ function generateButtons() {
         return;
     }
     const winningButtonIndex = Math.floor(Math.random() * num);
-    for (let i = 0; i < num; i++) {
+    for (let i = 0; i < num; ++i) {
         const button = document.createElement('button');
-        button.textContent = `Buton ${i + 1}`;
+        button.textContent = `Button ${i + 1}`;
         button.onclick = function() {
-            if (i === winningButtonIndex) {
-                button.textContent = "You win!";
-                confetti({
-                    particleCount: 2000,
-                    spread: 10,
-                    origin: { y: 0.0 },
-                });
-            } else {
-                button.textContent = "You lost";
-            }
+            handleButtonClick(i, winningButtonIndex);
         };
         container.appendChild(button);
     }
 }
 
-
+function handleButtonClick(clickedIndex, winningButtonIndex) {
+    if (clickedIndex === winningButtonIndex) {
+        this.innerHTML = "You win!";
+        confetti({
+            particleCount: 2000,
+            spread: 10,
+            origin: { y: 0.0 },
+        });
+    } else {
+        this.innerHTML = "You lose";
+    }
+}
+createButtons();
